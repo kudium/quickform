92fbca0e803f2127791e27d7de298accb3681752
samtax01 <samsoniyanu@hotmail.com>
2025-12-05 14:05:08 -0500

    fix: logout redirect should go home

--- public/pages/logout.php
+++ public/pages/logout.php
@@ -3,5 +3,5 @@ require_once __DIR__ . '/../../server/auth.php';
 require_once __DIR__ . '/../../server/utils.php';
 logoutUser();
 $baseUrl = web_base_url();
-header('Location: ' . $baseUrl . 'auth/login');
+header('Location: ' . $baseUrl . '');
 exit;
